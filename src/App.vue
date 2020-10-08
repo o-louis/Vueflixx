76edf295be7ce0df121e2b0a33e4cff947ce5478
Oriane <oriane.louis.pro@gmail.com>
2020-10-08 20:09:19 +0200

    Started integrating

--- src/App.vue
+++ src/App.vue
@@ -20,17 +20,38 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
+  $--fs--h2: clamp(2rem, 2vw, 5rem);
+
+  $black: #000;
+  $white: #fff;
+
   * {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+    font-family: 'Source Sans Pro', sans-serif;
+    list-style: none;
+  }
+
+  html {
+    font-size: 62.5%;
+  }
+
+  body {
+    background: $black;
+    color: $white;
   }
 
   img {
     max-width: 100%;
     height: auto;
     object-fit: cover;
+  }
+
+  a {
+    text-decoration: none;
+    color: white;
   }
 
   main {
--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,14 +1,16 @@
 <template>
     <nav>
-        <div class="logo">
-            <router-link :to="{ name: 'Home' }">vFlix</router-link>
-        </div>
+        <div class="nav">
+            <div class="logo">
+                <router-link :to="{ name: 'Home' }">vFlix</router-link>
+            </div>
 
-        <ul class="nav-list">
-            <li>
-                <router-link :to="{ name: 'Favorites' }">Favorites</router-link>
-            </li>
-        </ul>
+            <ul class="nav-list">
+                <li>
+                    <router-link :to="{ name: 'Favorites' }">Favorites</router-link>
+                </li>
+            </ul>
+        </div>
     </nav>
 </template>
 
@@ -18,4 +20,19 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+nav {
+    padding: 20px 0;
+  .nav {
+      margin: 0 auto;
+      max-width: 1200px;
+      width: 90%;
+      display: flex;
+      justify-content: space-between;
+
+      .logo {
+          font-size: 4rem;
+      }
+  }
+}
+</style>
